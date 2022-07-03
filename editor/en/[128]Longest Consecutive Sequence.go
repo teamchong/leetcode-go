d0e131a5a144eaa3718832466a81ae9d7a21022f
teamchong <teamchong+github@pm.me>
2022-07-03 11:42:33 +0800

    Logest Consecutive Sequence

---
+++ editor/en/[128]Longest Consecutive Sequence.go
@@ -0,0 +1,37 @@
+//Given an unsorted array of integers nums, return the length of the longest 
+//consecutive elements sequence. 
+//
+// You must write an algorithm that runs in O(n) time. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [100,4,200,1,3,2]
+//Output: 4
+//Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. 
+//Therefore its length is 4.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [0,3,7,2,5,8,4,6,0,1]
+//Output: 9
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 0 <= nums.length <= 10⁵ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// 
+// Related Topics Array Hash Table Union Find 👍 10338 👎 461
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func longestConsecutive(nums []int) int {
+    
+}
+//leetcode submit region end(Prohibit modification and deletion)
